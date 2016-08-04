899884c410a276223aed25f3c7cbe3f673b01bcf
Hoangelato <lhp.huyhoangnguyen@gmail.com>
2016-08-04 14:44:57 +0700

    player.client

--- app/src/main/java/com/example/hoangelato/coachridetodevilcastle/Player.java
+++ app/src/main/java/com/example/hoangelato/coachridetodevilcastle/Player.java
@@ -1,5 +1,7 @@
 package com.example.hoangelato.coachridetodevilcastle;
 
+import com.example.hoangelato.coachridetodevilcastle.Network.Client;
+
 import java.util.ArrayList;
 import java.util.Vector;
 
@@ -11,6 +13,7 @@ public class Player implements PlayerActions{
     private int team;
     private Occupation occupation;
     public ArrayList<Item> itemsList;
+    private Client mClient;
 //    public ArrayList<Player> teammatesList;
 //    public ArrayList<Player> opponentsList;
 //    public ArrayList<Item> teammateItems;
